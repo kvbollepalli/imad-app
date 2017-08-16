187344616a6375d6332c845e2163733358d2263a
kvbollepalli <30895179+kvbollepalli@users.noreply.github.com>
2017-08-16 23:00:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,13 +4,82 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+var articles = `
+var articleOne : {
+    
+    title: 'article one | krishnaveni ' , 
+    heading : 'article one ' , 
+    date : '16 aug ,  2017' , 
+    content : `<p>
+                This is the  content for my  first article .This is the  content for my  first article. 
+                This is the  content for my  first article .
+            </p>
+            <p>
+                This is the  content for my  first article .This is the  content for my  first article. 
+                This is the  content for my  first article .
+            </p>
+            <p>
+                This is the  content for my  first article .This is the  content for my  first article. 
+                This is the  content for my  first article .
+            </p> `
+    
+    
+    
+} , 
 
+`
+function createtemplate(data)
+{
+var title = data.title;
+var date = data.date;
+var heading = data.heading ;
+var content = data.content;
+var htmltemplate=`
+
+<html>
+<head>
+    <title>
+        ${title}
+    </title>
+    <meta name="viewport" content="width-device-width , vertical-scale-1"/>
+    <link href="/ui/style.css" rel="stylesheet" />
+  
+</head>
+<body>
+    
+    <div class="container">
+        <div>
+            
+            <a href="/"  > Home </a>
+            
+        </div>
+        <HR/>
+        <H3>
+           ${heading}
+        </H3>
+        <div>
+            ${date}
+        </div>
+        
+        <div>
+            ${ content }
+            
+        </div>
+    </div>
+</body>
+
+
+</html>
+
+`;
+return htmltemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one' , function(req, res){
     
-   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+   res.send(createtemplate(articleOne));
    
     
 });
