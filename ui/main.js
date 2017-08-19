def2edd4afd536823101e20d1f6a8de72f3928e6
kvbollepalli <30895179+kvbollepalli@users.noreply.github.com>
2017-08-19 15:50:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -38,6 +38,7 @@ submit.onClick = function(){
     // captur the list of names and render it as  a list
     
     var names = ['name1' , 'name2' , 'name3' , 'name4'] ;
+    var list='';
     
     for(var i = 0 ; i<names.length ; i++)
     {
