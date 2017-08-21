374195f0775c6d2aba4e89a5463e6345d951c476
kvbollepalli <30895179+kvbollepalli@users.noreply.github.com>
2017-08-21 14:55:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -116,7 +116,8 @@ app.get('/counter' , function(req,res){
 });
 
 var names=[];
-app.get('/submit-name' , function(req,res){
+//app.get('/submit-name/:name' , function(req,res){
+    app.get('/submit-name' , function(req,res){
     // get the name from the request 
     //var name = req.params.name ;
     var name = query.name;
